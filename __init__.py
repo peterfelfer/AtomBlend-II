94e91b4454062e01ab1d074d2ca2a5f73a2aafa6
qa43nawu <100777968+qa43nawu@users.noreply.github.com>
2022-03-16 13:57:28 +0100

    showing only percentage of atoms when loading file is now possible

--- __init__.py
+++ __init__.py
@@ -42,6 +42,14 @@ AtomBlendAddon.name = bl_info['name'] + " v" + '.'.join(str(v) for v in bl_info[
 if bpy.app.version < bl_info['blender']:
     raise Exception("This version of Blender is not supported by " + bl_info['name'] + ". Please use v" + '.'.join(str(v) for v in bl_info['blender']) + " or higher.")
 
+# ----------------- ADDON INITIALIZATION --------------------
+@persistent
+def atom_blend_addon_init_handler(dummy1, dummy2):
+    # load the panel variables
+    bpy.types.Scene.atom_blend_addon_settings = bpy.props.PointerProperty(type=AtomBlendAddonSettings)
+
+    # get the active window
+    AtomBlendAddon.BlenderWindow = bpy.context.window
 
 # ---------- ADDON INITIALIZATION & CLEANUP -------------
 def register():
@@ -55,6 +63,7 @@ def register():
     bpy.utils.register_class(ATOMBLEND_PT_panel_general)
     bpy.utils.register_class(ATOMBLEND_PT_panel_file)
 
+    bpy.app.handlers.load_post.append(atom_blend_addon_init_handler)
 
 def unregister():
     # unregister all classes of the addon
@@ -62,9 +71,14 @@ def unregister():
     # bpy.utils.unregister_class(AtomBlendAddonUI)
     bpy.utils.unregister_class(ATOMBLEND_OT_load_file)
 
+    # remove initialization helper app handler
+    bpy.app.handlers.load_post.remove(atom_blend_addon_init_handler)
+
     # UI elements
     if hasattr(bpy.types, "ATOMBLEND_PT_panel_general"): bpy.utils.unregister_class(ATOMBLEND_PT_panel_general)
     if hasattr(bpy.types, "ATOMBLEND_PT_panel_file"): bpy.utils.unregister_class(ATOMBLEND_PT_panel_file)
+    # delete all variables
+    if hasattr(bpy.types.Scene, "holo_addon_settings"): del bpy.types.Scene.holo_addon_settings
 
 # this should only be needed if we want to start the addon with the play button from within blender
 if __name__ == "__main__":
