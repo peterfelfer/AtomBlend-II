6fb55cdb470d1726ead3c84302c4a0ed49df6a47
qa43nawu <100777968+qa43nawu@users.noreply.github.com>
2022-03-18 13:59:42 +0100

    storing rrng file data in structure

--- ui.py
+++ ui.py
@@ -22,8 +22,6 @@ class AtomBlendAddonUI:
     def update_vertex_percentage(self, context):
         print('SELECT VERTEX PERCENTAGE')
 
-
-
 # Preferences pane for this Addon in the Blender preferences
 class AtomBlendAddonSettings(bpy.types.PropertyGroup):
     vertex_percentage: bpy.props.IntProperty(
@@ -84,41 +82,6 @@ class ATOMBLEND_PT_panel_rrng_file(bpy.types.Panel):
         else:
             loaded_row.label(text='No file loaded yet...')
 
-# Operators used for buttons
-class ATOMBLEND_OT_load_rrng_file(bpy.types.Operator):
-    bl_idname = "atom_blend_viewer.load_rrng_file"
-    bl_label = "Open file"
-    bl_description = "Load a file of the following types:\n.rrng"
-
-    filepath: bpy.props.StringProperty(subtype="FILE_PATH")
-    filter_glob: bpy.props.StringProperty(
-        default='*.rrng',
-        options={'HIDDEN'}
-    )
-
-    @classmethod
-    def poll(cls, context):
-        return True  # context.object is not None
-
-    def execute(self, context):
-        print("the selected filepath" + self.filepath)
-        AtomBlendAddon.path_rrng = self.filepath
-        # AtomBlendAddon.setup_scene()
-
-        if AtomBlendAddon.path_rrng.endswith('.rrng'):
-            AtomBlendAddon.load_rrng_file(self, context)
-
-        AtomBlendAddon.FileLoadedRRNG = True
-        print(f"Object Loaded: {AtomBlendAddon.FileLoadedRRNG}")
-
-        # https://docs.blender.org/api/current/bpy.types.Operator.html#calling-a-file-selector
-        return {'FINISHED'}
-
-    def invoke(self, context, event):
-        print("the selected filepath" + self.filepath)
-        context.window_manager.fileselect_add(self)
-        return {'RUNNING_MODAL'}
-
 class ATOMBLEND_PT_panel_file(bpy.types.Panel):
     bl_idname = "ATOMBLEND_PT_panel_file"  # unique identifier for buttons and menu items to reference.
     bl_label = "Load file"  # display name in the interface.
@@ -173,12 +136,16 @@ class ATOMBLEND_OT_load_file(bpy.types.Operator):
 
         # if there's already an object loaded we want to delete it so we can load another object
         if AtomBlendAddon.FileLoaded:
-            obj_to_delte = bpy.data.objects['Atoms']
-            bpy.data.objects.remove(obj_to_delte, do_unlink=True)
+            obj_to_delete = bpy.data.objects['Atoms']
+            bpy.data.objects.remove(obj_to_delete, do_unlink=True)
 
-        if AtomBlendAddon.path.endswith('.epos'):
+            # removing geometry nodes group
+            geometry_nodes_group = bpy.data.node_groups['Geometry Nodes']
+            bpy.data.node_groups.remove(geometry_nodes_group)
+
+        if AtomBlendAddon.path.lower().endswith('.epos'):
             AtomBlendAddon.load_epos_file(self, context)
-        elif AtomBlendAddon.path.endswith('.pos'):
+        elif AtomBlendAddon.path.lower().endswith('.pos'):
             AtomBlendAddon.load_pos_file(self, context)
 
         AtomBlendAddon.FileLoaded = True
@@ -189,6 +156,41 @@ class ATOMBLEND_OT_load_file(bpy.types.Operator):
         # bpy.data.screens["Modeling-nonnormal"].shading.type = "RENDERED"
 
         # bpy.ops.view3d.toggle_shading(type='RENDERED')
+
+        # https://docs.blender.org/api/current/bpy.types.Operator.html#calling-a-file-selector
+        return {'FINISHED'}
+
+    def invoke(self, context, event):
+        print("the selected filepath" + self.filepath)
+        context.window_manager.fileselect_add(self)
+        return {'RUNNING_MODAL'}
+
+class ATOMBLEND_OT_load_rrng_file(bpy.types.Operator):
+    bl_idname = "atom_blend_viewer.load_rrng_file"
+    bl_label = "Open file"
+    bl_description = "Load a file of the following types:\n.rrng"
+
+    filepath: bpy.props.StringProperty(subtype="FILE_PATH")
+    filter_glob: bpy.props.StringProperty(
+        default='*.rrng',
+        options={'HIDDEN'}
+    )
+
+    @classmethod
+    def poll(cls, context):
+        return True  # context.object is not None
+
+    def execute(self, context):
+        print("the selected filepath" + self.filepath)
+        AtomBlendAddon.path_rrng = self.filepath
+        # AtomBlendAddon.setup_scene()
+
+
+        if AtomBlendAddon.path_rrng.lower().endswith('.rrng'):
+            AtomBlendAddon.load_rrng_file(self, context)
+
+        AtomBlendAddon.FileLoadedRRNG = True
+        print(f"Object Loaded: {AtomBlendAddon.FileLoadedRRNG}")
 
         # https://docs.blender.org/api/current/bpy.types.Operator.html#calling-a-file-selector
         return {'FINISHED'}
