643d144304546d9a036feca3ccc6f6001d49a7c8
qa43nawu <100777968+qa43nawu@users.noreply.github.com>
2022-06-14 15:28:51 +0200

    checkboxes for hiding elements

--- shaders.py
+++ shaders.py
@@ -6,7 +6,6 @@ class ABShaders:
             uniform mat4 object_matrix;
             uniform float point_size;
             uniform float alpha_radius;
-            uniform float global_alpha;
             out vec4 f_color;
             out float f_alpha_radius;
             void main()
@@ -14,8 +13,7 @@ class ABShaders:
                 gl_Position = perspective_matrix * object_matrix * vec4(position, 1.0f);
                 gl_PointSize = point_size;
                 // f_color = color;
-                f_color = vec4(color[0], color[1], color[2], global_alpha);
-                //f_color = vec4(color[0], color[0], color[0], global_alpha);
+                f_color = vec4(color[0], color[1], color[2], color[3]);
                 f_alpha_radius = alpha_radius;
             }
         '''
