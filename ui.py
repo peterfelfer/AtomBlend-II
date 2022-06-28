26ce3e619bf7a5a64515890eabb8ebbf0b1ba95d
juliane-re <juliane_re@yahoo.de>
2022-06-28 14:50:18 +0200

    code cleanup

--- ui.py
+++ ui.py
@@ -28,11 +28,8 @@ class AtomBlendAddonSettings(bpy.types.PropertyGroup):
         # reset color list
         ABGlobals.atom_color_list = []
 
-        print(ABGlobals.all_elements_by_name)
-        print(bpy.context.scene.color_settings)
         for elem_name in ABGlobals.all_elements_by_name:
             elem_amount = ABGlobals.all_elements_by_name[elem_name]['num_of_atoms']
-            print(elem_name, elem_amount)
 
             col_struct = bpy.context.scene.color_settings[elem_name].color
             col = (col_struct[0], col_struct[1], col_struct[2], col_struct[3])
@@ -40,10 +37,7 @@ class AtomBlendAddonSettings(bpy.types.PropertyGroup):
 
         # flatten list: e.g. [[(1,1,0,1), (0,0,1,1)], []] -> [(1,1,0,1), (0,0,1,1)]
         if isinstance(ABGlobals.atom_color_list[0], list):
-            print('CU PRE', ABGlobals.atom_color_list)
             ABGlobals.atom_color_list = [x for xs in ABGlobals.atom_color_list for x in xs]  # https://stackoverflow.com/questions/952914/how-do-i-make-a-flat-list-out-of-a-list-of-lists
-            print('CU POST', ABGlobals.atom_color_list)
-
 
     def alpha_update(self, context):
         # reset color list
@@ -51,7 +45,6 @@ class AtomBlendAddonSettings(bpy.types.PropertyGroup):
 
         for elem_name in ABGlobals.all_elements_by_name:
             elem_amount = ABGlobals.all_elements_by_name[elem_name]['num_of_atoms']
-            print(elem_name, elem_amount)
 
             col_struct = bpy.context.scene.color_settings[elem_name].color
             if bpy.context.scene.color_settings[elem_name].display:
@@ -62,15 +55,10 @@ class AtomBlendAddonSettings(bpy.types.PropertyGroup):
 
         # flatten list: e.g. [[(1,1,0,1), (0,0,1,1)], []] -> [(1,1,0,1), (0,0,1,1)]
         if isinstance(ABGlobals.atom_color_list[0], list):
-            print('AU PRE', ABGlobals.atom_color_list)
             ABGlobals.atom_color_list = [x for xs in ABGlobals.atom_color_list for x in xs]  # https://stackoverflow.com/questions/952914/how-do-i-make-a-flat-list-out-of-a-list-of-lists
-            print('AU POST', ABGlobals.atom_color_list)
-
 
     def update_point_size(self, context):
         ABGlobals.point_size = context.scene.atom_blend_addon_settings.point_size
-        print('')
-
 
     # properties
     vertex_percentage: bpy.props.FloatProperty(
@@ -94,21 +82,11 @@ class AtomBlendAddonSettings(bpy.types.PropertyGroup):
         update=update_point_size
     )
 
-    # material_settings: bpy.props.FloatVectorProperty(
-    #     name="Material",
-    #     min=0.0,
-    #     max=1.0,
-    #     subtype="COLOR",
-    #     size=4,
-    #     # update=AtomBlendAddonUI.update_background,
-    # )
-
     # for debug purposes
     debug_automatic_file_loading: bpy.props.BoolProperty(
         name='Automatic file loading',
         default=True,
     )
-
 
     debug_dataset_selection: bpy.props.EnumProperty(
         name='Dataset Selection',
@@ -118,8 +96,6 @@ class AtomBlendAddonSettings(bpy.types.PropertyGroup):
         default='T:\Heller\AtomBlendII\EisenKorngrenze\R56_03446-v01',
     )
 
-
-
 class ATOMBLEND_PT_panel_general(bpy.types.Panel):
     bl_idname = "ATOMBLEND_PT_panel_general"  # unique identifier for buttons and menu items to reference.
     bl_label = "AtomBlend-II"  # display name in the interface.
@@ -127,15 +103,8 @@ class ATOMBLEND_PT_panel_general(bpy.types.Panel):
     bl_region_type = "UI"
     bl_category = "AtomBlend-II"
 
-    # @classmethod
-    # def poll(cls, context):
-    #     # the panel should always be drawn
-    #     return True
-
     def draw(self, context):
         layout = self.layout
-        column = layout.column()
-        # column.label(text=".epos file")
 
 class ATOMBLEND_PT_panel_rrng_file(bpy.types.Panel):
     bl_idname = "ATOMBLEND_PT_panel_rrng_file"  # unique identifier for buttons and menu items to reference.
@@ -214,24 +183,6 @@ class MaterialSetting(bpy.types.PropertyGroup):
     color: bpy.props.FloatVectorProperty(name="", subtype='COLOR', min=0.0, max=1.0, size=4, default=(1.0, 0.0, 0.0, 1.0), update=AtomBlendAddonSettings.color_update)
     display: bpy.props.BoolProperty(name="", default=True, update=AtomBlendAddonSettings.alpha_update)
 
-    # list = bpy.props.CollectionProperty(type=MaterialSetting)
-    print('mat settings')
-    # print('list: ', list)
-    # def fill_to(self):
-    # list.add()
-    print('fill to')
-
-# def test(self, context):
-#     cube1_item = bpy.context.scene.my_settings.add()
-#     cube1_item.name = 'Cube1'
-#     cube1_item.value = 100
-#
-#     cube2_item = bpy.context.scene.my_settings.add()
-#     cube2_item.name = 'Cube2'
-#     cube2_item.value = 5
-
-
-
 class ATOMBLEND_PT_shader_color_settings(bpy.types.Panel):
     bl_idname = "ATOMBLEND_PT_shader_color_settings"  # unique identifier for buttons and menu items to reference.
     bl_label = "Color settings"  # display name in the interface.
@@ -286,87 +237,6 @@ class ATOMBLEND_PT_shader_color_settings(bpy.types.Panel):
             text_row = col.row()
             text_row.label(text='Load .epos/.pos and .rrng file')
 
-
-
-
-class ATOMBLEND_PT_color_settings(bpy.types.Panel):
-    bl_idname = "ATOMBLEND_PT_color_settings"  # unique identifier for buttons and menu items to reference.
-    bl_label = "Color settings"  # display name in the interface.
-    bl_space_type = "VIEW_3D"
-    bl_region_type = "UI"
-    bl_category = "AtomBlend-II"
-    bl_parent_id = "ATOMBLEND_PT_panel_general"
-
-    @classmethod
-    def poll(cls, context):
-        return True  # context.object is not None
-
-    def draw(self, context):
-        layout = self.layout
-        # only draw if both files are loaded
-        # ll = bpy.props.CollectionProperty(type=MaterialSetting)
-        # ll.add()
-        # box = layout.box()
-        # element_row = box.row()
-
-        if ABGlobals.FileLoaded_rrng and ABGlobals.FileLoaded_e_pos:
-
-            # make one top row for labeling
-            col = layout.column(align=True)
-            row = col.row(align=True)
-            # row = col.row(align=True)
-            name_col = row.column(align=True)
-            charge_col = row.column(align=True)
-            color_col = row.column(align=True)
-
-            name_col.label(text='Element')
-            charge_col.label(text='Charge')
-            color_col.label(text='Color')
-
-            col.separator()
-
-            for obj in bpy.data.objects:
-                obj_mats = [m.material for m in obj.material_slots]
-                for mat in obj_mats:
-
-                    # add unknown element at the end because elements are stored alphabetically
-                    if mat.name == 'unknown_element':
-                        continue
-
-                    if mat and mat.use_nodes:
-                        bsdf = mat.node_tree.nodes.get("Principled BSDF")
-                        row = col.row(align=True)
-                        name_col = row.column(align=True)
-                        charge_col = row.column(align=True)
-                        color_col = row.column(align=True)
-
-                        splitted_name = mat.name.split('_')
-
-                        name_col.label(text=splitted_name[0])
-                        charge_col.label(text=splitted_name[1])
-
-                        color_col.prop(bsdf.inputs['Base Color'], "default_value", text='')
-
-            # add unknown element to the end of the list
-            mat = bpy.data.objects['unknown_element'].material_slots[0].material
-            if mat and mat.use_nodes:
-                bsdf = mat.node_tree.nodes.get("Principled BSDF")
-                row = col.row(align=True)
-                name_col = row.column(align=True)
-                charge_col = row.column(align=True)
-                color_col = row.column(align=True)
-
-                name_col.label(text='Unknown')
-                charge_col.label(text='n/a')
-                color_col.prop(bsdf.inputs['Base Color'], "default_value", text='')
-
-        else:
-            col = layout.column(align=True)
-            text_row = col.row()
-            # text_row.label(text='Load .epos/.pos and .rrng file')
-            text_row.label(text='Load .epos/.pos and .rrng file')
-
-
 class ATOMBLEND_PT_panel_debug(bpy.types.Panel):
     bl_idname = "ATOMBLEND_PT_panel_debug"  # unique identifier for buttons and menu items to reference.
     bl_label = "DEBUG"  # display name in the interface.
@@ -384,10 +254,6 @@ class ATOMBLEND_PT_panel_debug(bpy.types.Panel):
         col = layout.column(align=True)
         col.prop(context.scene.atom_blend_addon_settings, 'debug_dataset_selection')
         col.prop(context.scene.atom_blend_addon_settings, 'debug_automatic_file_loading')
-        print(context.scene.atom_blend_addon_settings.debug_dataset_selection)
-
-
-
 
 # Operators used for buttons
 class ATOMBLEND_OT_load_file(bpy.types.Operator):
@@ -407,7 +273,6 @@ class ATOMBLEND_OT_load_file(bpy.types.Operator):
 
     def execute(self, context):
         ABGlobals.path = self.filepath
-        # ABGlobals.setup_scene()
 
         # if there's already an object loaded we want to delete it so we can load another object
         if ABGlobals.FileLoaded_e_pos:
@@ -422,17 +287,9 @@ class ATOMBLEND_OT_load_file(bpy.types.Operator):
         ABGlobals.FileLoaded_e_pos = True
         print(f"Object Loaded: {ABGlobals.FileLoaded_e_pos}")
 
-        # print(bpy.data.screens["Modeling-nonnormal"].shading.type)
-
-        # bpy.data.screens["Modeling-nonnormal"].shading.type = "RENDERED"
-
-        # bpy.ops.view3d.toggle_shading(type='RENDERED')
-
-        # https://docs.blender.org/api/current/bpy.types.Operator.html#calling-a-file-selector
         return {'FINISHED'}
 
     def invoke(self, context, event):
-        print(self)
         if context.scene.atom_blend_addon_settings.debug_automatic_file_loading:
             self.filepath = context.scene.atom_blend_addon_settings.debug_dataset_selection + '.epos'
             return self.execute(context)
@@ -457,7 +314,6 @@ class ATOMBLEND_OT_load_rrng_file(bpy.types.Operator):
 
     def execute(self, context):
         ABGlobals.path_rrng = self.filepath
-        # ABGlobals.setup_scene()
 
         if ABGlobals.path_rrng.lower().endswith('.rrng'):
             AtomBlendAddon.load_rrng_file(self, context)
