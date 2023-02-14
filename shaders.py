7583ae12c4de5bb925ea0d3c63ca1f95d6f3704c
Juliane Reithmeier <100777968+juliane-re@users.noreply.github.com>
2023-02-14 15:28:38 +0100

    rendering red circle at bottom left corner for viewport and rendered image

--- shaders.py
+++ shaders.py
@@ -36,7 +36,7 @@ class ABShaders:
                 f_color = vec4(color[0], color[1], color[2], color[3]);
                 f_alpha_radius = 1.0;
             }
-        '''
+    '''
 
     fragment_shader_simple = '''
             in vec4 f_color;
@@ -56,4 +56,41 @@ class ABShaders:
                 }
                 fragColor = vec4(f_color.rgb, f_color.a * a);
             }
-       '''
+    '''
+
+    legend_vertex_shader = '''
+            in vec3 position;
+            in vec4 color;
+            in float ps;
+            //uniform mat4 projection_matrix;
+            //uniform mat4 object_matrix;
+            out vec4 f_color;
+            out float f_alpha_radius;
+            void main()
+            {
+                gl_Position = vec4(position, 1.0f);
+                gl_PointSize = ps;
+                f_color = vec4(color[0], color[1], color[2], color[3]);
+                f_alpha_radius = 1.0;
+            }
+    '''
+
+    legend_fragment_shader = '''
+            in vec4 f_color;
+            in float f_alpha_radius;
+            out vec4 fragColor;
+            void main()
+            {
+                float r = 0.0f;
+                float d = 0.0f;
+                float a = 1.0f;
+                vec2 cxy = 2.0f * gl_PointCoord - 1.0f;
+                r = dot(cxy, cxy);
+                d = fwidth(r);
+                a = 1.0 - smoothstep(1.0 - (d / 2), 1.0 + (d / 2), r);
+                if(r > 0.6){
+                    discard;
+                }
+                fragColor = vec4(f_color.rgb, f_color.a * a);
+            }
+    '''
