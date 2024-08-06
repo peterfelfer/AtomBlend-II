eb596c46796eebd1ad5f9e8d4114d6b20761f59e
juliane-re <juliane_re@fau.de>
2024-08-06 14:46:42 +0200

    gaussianBall code cleanup

--- gaussian_splatting/submodules/diff-gaussian-rasterization/cuda_rasterizer/forward.cu
+++ gaussian_splatting/submodules/diff-gaussian-rasterization/cuda_rasterizer/forward.cu
@@ -821,8 +821,6 @@ render_gaussianBall(
 	const float* projmatrix,
 	const float* orig_points,
 	const float scale_modifier,
-    int* radii,
-    int* radii_xy,
 	float* __restrict__ out_color)
 {
     // Identify current tile and associated min/max pixel range.
@@ -887,9 +885,6 @@ render_gaussianBall(
             float4 con_o = collected_conic_opacity[j];
             float power = -0.5f * (con_o.x * d.x * d.x + con_o.z * d.y * d.y) - con_o.y * d.x * d.y;
             if (power > 0.0f){
-                C[0] = 0.0f;
-                C[1] = 1.0f;
-                C[2] = 0.0f;
                 continue;
             }
 
@@ -919,70 +914,16 @@ render_gaussianBall(
 
             float radius = scale_modifier;
 
-            float r_in_pixels = float(radii[collected_id[j]]); // the size of the radius in pixels
-
-            float r_in_pixels_x = float(radii_xy[collected_id[j] + 1]);
-            float r_in_pixels_y = float(radii_xy[collected_id[j]]);
-
-            if (r_in_pixels_x == 0.0f || r_in_pixels_y == 0.0f){
-//                printf("CONTINUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUE");
-                C[0] = 0.0f;
-                C[1] = 1.0f;
-                C[2] = 0.0f;
-                continue;
-            }
-
-            glm::vec2 reltc = glm::vec2(d.x, d.y) * 2.0f - 1.0f;
-
-//            printf("%f, %f d.x, d.y", d.x / r_in_pixels_x, d.y / r_in_pixels_y);
-//            reltc.x = reltc.x;// * r_in_pixels_x;
-//            reltc.y = reltc.y;// * r_in_pixels_y;
-            float dist_to_center = sqrt(reltc.x * reltc.x + reltc.y * reltc.y);
-            dist_to_center = dist_to_center / r_in_pixels;
-
-
-            // Calculate the surface normal
-//            float dx = (reltc.x / r_in_pixels_x);  // X-distance from the pixel to the sphere center
-//            float dy = (reltc.y / r_in_pixels_y); // Y-distance from the pixel to the sphere center
-
-            // Check if point is in the gaussian (ellipse)
-            float normalized_x = reltc.x / r_in_pixels_x;
-            float normalized_y = reltc.y / r_in_pixels_y;
-
-            bool inside_ellipse = (normalized_x * normalized_x + normalized_y * normalized_y) <= 1.0f;
-
-//            C[0] = con_o.x * d.x * d.x;
-//            C[1] = con_o.z * d.y * d.y;
-//            C[2] = 0;
-//            T = test_T;
-
-            inside_ellipse = exp(power) > 0.01f;
-
-            if (inside_ellipse) {  // Check if the pixel is inside the sphere
-//                float dz = sqrtf(radius - dist_to_center);
-//                float dz = sqrtf(radius * radius - glm::dot(reltc, reltc));
-//                float dz = sqrtf(radius * radius - (dx * dx + dy * dy));
-//                float dz = sqrtf(radius - dist_to_center);
+            bool inside_ellipse = exp(power) > 0.01f;
+            if (inside_ellipse) {
                 float dz = exp(0.35 * power);
 
                 C[0] += features[collected_id[j] * CHANNELS] * alpha * dz * T;
                 C[1] += features[collected_id[j] * CHANNELS + 1] * alpha * dz * T;
                 C[2] += features[collected_id[j] * CHANNELS + 2] * alpha * dz * T;
 
-//                C[0] = exp(power);
-//                C[1] = 0.0f;
-//                C[2] = 0.0f;
-//                C[3] = 1.0f;
-
                 T = test_T;
             }
-            else {
-                C[0] = 1.0f;
-                C[1] = 0.0f;
-                C[2] = 0.0f;
-                T = test_T;
-            }
-
 
             // Keep track of last range entry to update this
             // pixel.
@@ -1278,8 +1219,6 @@ void FORWARD::render(int P,
             projmatrix,
             orig_points,
             scale_modifier,
-            radii,
-            radii_xy,
             out_color
         );
     } else if (render_mode == 4) { // gaussian ball opt
