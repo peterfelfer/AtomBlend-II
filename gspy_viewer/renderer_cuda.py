1734c0a0e07fcb2c7ad80a03c6f58ed5df4ea889
juliane-re <juliane_re@fau.de>
2024-09-10 10:37:14 +0200

    backup before individual opactiy parameter

--- gspy_viewer/renderer_cuda.py
+++ gspy_viewer/renderer_cuda.py
@@ -131,8 +131,7 @@ class CUDARenderer(GaussianRenderBase):
             "debug": False,
             "render_mode": 0,
             "index_properties": torch.Tensor([]),
-            "orthographic_cam": False,
-            "individual_opacity_intensity": 1.0,
+            "orthographic_cam": False
         }
         gl.glViewport(0, 0, w, h)
         self.program = util.compile_shaders(VERTEX_SHADER_SOURCE, FRAGMENT_SHADER_SOURCE)
